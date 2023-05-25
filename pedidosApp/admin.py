335987088b73508648f1bc2d5088340ed6bc3093
Ruben Lopez <rubenlopsol86@gmail.com>
2023-05-25 19:20:53 +0200

    trabajando en carrito, y pedidos:boton de compra, siguiente= vistas

--- pedidosApp/admin.py
+++ pedidosApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pedido, Linea_Pedido
 
 # Register your models here.
+
+# class PedidoAdmin(admin.ModelAdmin):
+#     pass
+
+# class Linea_PedidoAdmin(admin.ModelAdmin):
+#     pass
+
+# admin.site.register(Pedido, PedidoAdmin)
+# admin.site.register(Linea_Pedido, Linea_PedidoAdmin)
+
+admin.site.register([Pedido, Linea_Pedido])
